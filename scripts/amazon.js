533d0ed6f819e09ee0b3ecb016758fd1653fdd01
Ryu <ryudhis.id@gmail.com>
2023-09-16 22:18:41 +0700

    add-to-cart-1

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product)=>{
         Added
         </div>
 
-        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+        <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
         Add to Cart
         </button>
     </div>`;
@@ -55,6 +55,23 @@ products.forEach((product)=>{
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click', ()=>{
-        console.log(button.dataset);
+        const productId = button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId === item.productId){
+                matchingItem=item;
+            }
+        });
+        
+        if(matchingItem){
+            matchingItem.quantity++;
+        }
+        else{
+            cart.push({
+                productId: productId,
+                quantity: 1
+            });
+        }
+        console.log(cart);
     });
 });
