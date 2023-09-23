2cf2dd8c7873330ded04a8a6714d59618bd3ecd9
Ryu <ryudhis.id@gmail.com>
2023-09-23 21:32:50 +0700

    add quantity selector function

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product)=>{
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -55,6 +55,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click', ()=>{
         const productId = button.dataset.productId;
+        let itemQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
         let matchingItem;
         cart.forEach((item)=>{
             if(productId === item.productId){
@@ -63,18 +64,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
         });
         
         if(matchingItem){
-            matchingItem.quantity++;
+            matchingItem.quantity+=itemQuantity;
         }
         else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: itemQuantity
             });
         }
         let cartQuantity = 0;
         cart.forEach((item)=>{
             cartQuantity+=item.quantity;
         })
+        console.log(cartQuantity)
         document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
